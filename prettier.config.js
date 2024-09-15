d30ac960b2205f2fc684e397e150841a599a2153
lmmqxyx <1368470804@qq.com>
2024-09-15 23:55:03 +0800

    initialize the project

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  singleQuote: false,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  printWidth: 100,
+  semi: false,
+  tabWidth: 2
+  // plugins: ['prettier-plugin-tailwindcss']
+}
